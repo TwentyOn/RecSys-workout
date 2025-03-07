6ab6cd171697c9fc0d9d7e58abb2d3a2bfd06761
Alexey <95855844+TwentyOn@users.noreply.github.com>
2025-03-07 18:26:44 +0500

    Delete recomendationSystems/test2.py

--- recomendationSystems/test2.py
+++
@@ -1,6 +0,0 @@
-from random import randrange
-
-pref_matrix = [[randrange(0, 5) for _ in range(15)] for _ in range(15)]
-PRODUCTS = {p: f'P{p + 1}' for p in range(len(pref_matrix))}
-
-print(PRODUCTS)
